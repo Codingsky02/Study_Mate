85d049cafc61e7de56fe5afedfba1a217f16fc66
VanshTheProgrammer <codingsky02@gmail.com>
2026-08-25 23:15:38 +0530

    connected Ti Db

--- db.py
+++ db.py
@@ -1,2 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+
+Database_url = "mysql+mysqldb://3ckRHqDbEegrMBt.root:l3GviezaL6VoMGIE@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
+
+Engine = create_engine(
+    Database_url,
+    pool_pre_ping=True,
+    connect_args={
+        "ssl" :{
+            "ssl":True,
+        }
+    }
+)
+
+SessionLocal = sessionmaker(bind=Engine)
+
+Base = declarative_base()
